a2d9814b5b1a797756e7b7f5938e813a57277efb
haoxingwei <haoxingwei@eastmoney.com>
2026-04-25 20:02:21 +0800

    feat: add localStorage persistence and drag-and-drop integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import { TodoInput } from "@/components/todo-input"
 import { TodoList } from "@/components/todo-list"
 import { TodoSidebar } from "@/components/todo-sidebar"
 
-function App() {
+function AppContent() {
   const [isDragging, setIsDragging] = useState(false)
 
   return (
+    <div className="mx-auto flex min-h-screen max-w-2xl flex-col gap-6 px-4 py-6 sm:px-6 sm:py-8">
+      <TodoInput isDragging={isDragging} />
+      <TodoList isDragging={isDragging} onDragStateChange={setIsDragging} />
+      <TodoSidebar />
+    </div>
+  )
+}
+
+function App() {
+  return (
     <Provider>
-      <div className="mx-auto flex min-h-screen max-w-2xl flex-col gap-6 px-4 py-6 sm:px-6 sm:py-8">
-        <TodoInput isDragging={isDragging} />
-        <TodoList isDragging={isDragging} onDragStateChange={setIsDragging} />
-        <TodoSidebar />
-      </div>
+      <AppContent />
     </Provider>
   )
 }
--- src/atoms/todo-atoms.ts
+++ src/atoms/todo-atoms.ts
@@ -4,9 +4,26 @@ import type { Todo } from "@/types/todo"
 import type { TodoRepository } from "@/repositories/todo-repository"
 import { LocalStorageTodoRepository } from "@/repositories/local-storage-todo-repository"
 
+const STORAGE_KEY = "oh-my-todo"
+
 const repo: TodoRepository = new LocalStorageTodoRepository()
 
-export const todosAtom = atomWithImmer<Todo[]>(repo.getAll())
+const innerTodosAtom = atomWithImmer<Todo[]>(repo.getAll())
+
+function persist(todos: Todo[]) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+}
+
+type TodoUpdate = Todo[] | ((draft: Todo[]) => void)
+
+export const todosAtom = atom<Todo[], [TodoUpdate], void>(
+  (get) => get(innerTodosAtom),
+  (get, set, arg: TodoUpdate) => {
+    set(innerTodosAtom, arg as never)
+    persist(get(innerTodosAtom))
+  },
+)
+
 export const selectedTodoIdAtom = atom<string | null>(null)
 
 export const selectedTodoAtom = atom((get) => {
